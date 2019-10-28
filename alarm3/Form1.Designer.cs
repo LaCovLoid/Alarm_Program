19c3e6392c30366e0952736df744134a168051ec
LaCovLoid <46214120+LaCovLoid@users.noreply.github.com>
2019-10-29 00:57:40 +0900

    Add files via upload

---
+++ alarm3/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace alarm3 {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "현재시간";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "알람 시간";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "2016년",
+            "2017년"});
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "알람 설정";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 96);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(80, 23);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Visible = false;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(12, 125);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(80, 23);
+            this.comboBox3.TabIndex = 10;
+            this.comboBox3.Visible = false;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(12, 154);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(80, 23);
+            this.comboBox4.TabIndex = 11;
+            this.comboBox4.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "노래 선택";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(161, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 91);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "노래 경로";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 509);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 29);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "멈추기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 538);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 29);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "더 들을래";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(211, 486);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 28);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "노래 듣기";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Location = new System.Drawing.Point(9, 477);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 90);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "1.노래를 선택합니다.\r\n2.알람 시간을 맞춘후\r\n알람설정을 누릅니다.\r\n3.창을 내리고 다른일을\r\n하고 계시다 보면\r\n4.제 시간에 알람이!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::alarm3.Properties.Resources.KakaoTalk_20160702_011253539;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(327, 583);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ alarm3/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace alarm3 {
+    public partial class Form1 : Form {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        private SoundPlayer Player = new SoundPlayer();
+
+        public Form1() {
+            InitializeComponent();
+            
+            System.DateTime.Now.ToString("yyyy");
+            DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
+
+
+            //날짜 추가-1
+            for (int m = 1; m < 13; m++) {
+                if (m < 10) {
+                    comboBox1.Items.Add("0" + m + "월");
+                } else comboBox1.Items.Add(m + "월");
+            }
+
+            //날짜 추가-1
+            for (int d = 1; d < 31; d++) {
+                if (d < 10) {
+                    comboBox2.Items.Add("0" + d + "일");
+                } else comboBox2.Items.Add(d + "일");
+            }
+
+            //시간 추가-1
+            for (int h = 0; h < 24; h++) {
+                if (h < 10) {
+                    comboBox3.Items.Add("0" + h + "시");
+                } else comboBox3.Items.Add(h + "시");
+            }
+
+            //시간추가-2
+            for (int m = 0; m < 60; m++) {
+                if (m < 10) {
+                    comboBox4.Items.Add("0" + m+ "분");
+                } else comboBox4.Items.Add(m + "분");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            timer1_Tick(sender, e);
+            timer1.Interval = 1000; 
+            timer1.Start(); 
+        }
+        private void timer1_Tick(object sender, EventArgs e) {
+            string now = DateTime.Now.ToString("yyyy" + "년" + "MM" + "월" + "dd" + "일" + "HH" + "시" + "mm" + "분" + "ss" + "초");
+            foreach (System.Diagnostics.Process procName in System.Diagnostics.Process.GetProcesses()) {
+                label1.Text = "현재시간은 " + now + "입니다";
+            }
+            string b = "2016년" + comboBox1.Text + comboBox2.Text + comboBox3.Text + comboBox4.Text + "00초";
+            string filename = label4.Text;
+            if ( now == b) {
+
+                Form2 Nyang = new Form2();
+                Nyang.Show();
+                mciSendString(("open \"" + filename + "\" type mpegvideo alias MediaFile"), null, 0, IntPtr.Zero);
+                mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+                //try {
+                //    this.Player.SoundLocation = @"filename";
+                //    this.Player.PlayLooping();
+                //} catch (Exception w) {
+                //}
+
+            }
+            if (comboBox1.Text == "01월" || comboBox1.Text == "03월" || comboBox1.Text == "05월" || comboBox1.Text == "07월" || comboBox1.Text == "09월" || comboBox1.Text == "11월") {
+                if (comboBox2.Items.Count == 30) {
+                    comboBox2.Items.Add("31일");
+                }
+                if (comboBox2.Items.Count == 28) {
+                    comboBox2.Items.Add("29일");
+                    comboBox2.Items.Add("30일");
+                    comboBox2.Items.Add("31일");
+                }
+            } else comboBox2.Items.Remove("31일");
+
+            if (comboBox1.Text == "02월") {
+                if (comboBox2.Items.Count == 30) {
+                    comboBox2.Items.Remove("29일");
+                    comboBox2.Items.Remove("30일");
+                }
+                if (comboBox2.Items.Count == 31) {
+                    comboBox2.Items.Remove("29일");
+                    comboBox2.Items.Remove("30일");
+                    comboBox2.Items.Remove("31일");
+                }
+            }
+            if (comboBox1.Text != "") {
+                comboBox2.Visible = true;
+            }
+            if (comboBox2.Text != "") {
+                comboBox3.Visible = true;
+            }
+            if (comboBox3.Text != "") {
+                comboBox4.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            string b = comboBox1.Text + comboBox2.Text + comboBox3.Text + comboBox4.Text;
+            label2.Text = "알람시간은 " + b + "입니다."; 
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+            label4.Text = openFileDialog1.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            mciSendString("stop MediaFile", null, 0, IntPtr.Zero);
+            this.Player.Stop();
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            string filename = label4.Text;
+            mciSendString(("open \"" + filename + "\" type mpegvideo alias MediaFile"), null, 0, IntPtr.Zero);
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+        }
+
+    }
+}
